737b44e1ab17a8fe0961330a5510156e5484e021
PatCG-WebDev <>
2024-05-24 11:46:27 +0200

    depouración de rutas

--- routes/web.php
+++ routes/web.php
@@ -30,86 +30,91 @@ Route::get('no-autorizado', function(){
 });
 
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////
 Route::middleware(['auth', 'verified'])->group(function () {
 
-    // ADMINISTRATOR
+    // Rutas para el administrador
     Route::middleware('can:administratorAccess')->group(function () {
+        // Rutas para la gestión de usuarios
+        Route::prefix('admin/users')->group(function () {
+            Route::get('/', [UserController::class, 'showUsers'])->name('administrator.show_users');
+            Route::get('add', [UserController::class, 'addUserForm'])->name('administrator.add_user_form');
+            Route::post('add', [UserController::class, 'addUser'])->name('administrator.add_user');
+            Route::get('edit/{userId}', [UserController::class, 'showEditUsersForm'])->name('administrator.edit_user');
+            Route::put('update', [UserController::class, 'updateUsers'])->name('administrator.update_user');
+            Route::delete('delete/{userId}', [UserController::class, 'deleteUser'])->name('administrator.delete_user');
+        });
 
-    // Rutas para la gestión de usuarios 
-        Route::get('admin/users', [UserController::class, 'showUsers'])->name('administrator.show_users');
-        Route::get('admin/users/add',  [UserController::class, 'addUserForm'])->name('administrator.add_user_form');
-        Route::post('admin/users/add',  [UserController::class, 'addUser'])->name('administrator.add_user');
-        Route::get('admin/users/edit/{userId}', [UserController::class, 'showEditUsersForm'])->name('administrator.edit_user');
-        Route::put('admin/update-users', [UserController::class, 'updateUsers'])->name('administrator.update_user');
-        Route::post('admin/save-users', [UserController::class, 'saveUsers'])->name('administrator.save_user');
-        Route::delete('admin/users/delete/{userId}', [UserController::class, 'deleteUser'])->name('administrator.delete_user');
+        // Rutas para la gestión de perfiles
+        Route::prefix('admin/profiles')->group(function () {
+            Route::get('/', [ProfileController::class, 'showProfiles'])->name('administrator.show_profiles');
+            Route::get('add', [ProfileController::class, 'addProfileForm'])->name('administrator.add_profile_form');
+            Route::post('add', [ProfileController::class, 'addProfile'])->name('administrator.add_profile');
+            Route::get('edit/{profileId}', [ProfileController::class, 'showEditProfileForm'])->name('administrator.edit_profile');
+            Route::put('update', [ProfileController::class, 'updateProfile'])->name('administrator.update_profile');
+            Route::delete('delete/{profileId}', [ProfileController::class, 'deleteProfile'])->name('administrator.delete_profile');
+        });
 
-    // Rutas para la gestión de perfiles
-        Route::get('admin/profiles', [ProfileController::class, 'showProfiles'])->name('administrator.show_profiles');
-        Route::get('admin/profiles/add',  [ProfileController::class, 'addProfileForm'])->name('administrator.add_profile_form');
-        Route::post('admin/profiles/add',  [ProfileController::class, 'addProfile'])->name('administrator.add_profile');
-        Route::get('admin/profiles/edit/{profileId}', [ProfileController::class, 'showEditProfileForm'])->name('administrator.edit_profile');
-        Route::put('admin/update-profile', [ProfileController::class, 'updateProfile'])->name('administrator.update_profile');    
-        Route::delete('admin/profiles/delete/{profileId}', [ProfileController::class, 'deleteProfile'])->name('administrator.delete_profile');    
+        // Rutas para la gestión de cursos
+        Route::prefix('admin/courses')->group(function () {
+            Route::get('/', [CourseController::class, 'showCourses'])->name('administrator.show_courses');
+            Route::get('add', [CourseController::class, 'addCourseForm'])->name('administrator.add_course_form');
+            Route::post('add', [CourseController::class, 'addCourse'])->name('administrator.add_course');
+            Route::get('edit/{courseId}', [CourseController::class, 'showEditCourseForm'])->name('administrator.edit_course');
+            Route::put('update', [CourseController::class, 'updateCourse'])->name('administrator.update_course');
+            Route::delete('delete/{courseId}', [CourseController::class, 'deleteCourse'])->name('administrator.delete_course');
+        });
 
-
-    // Rutas para la gestión de cursos
-        Route::get('admin/courses', [CourseController::class, 'showCourses'])->name('administrator.show_courses');
-        Route::get('admin/courses/add', [CourseController::class, 'addCourseForm'])->name('administrator.add_course_form');
-        Route::post('admin/courses/add', [CourseController::class, 'addCourse'])->name('administrator.add_course');
-        Route::get('admin/courses/edit/{courseId}', [CourseController::class, 'showEditCourseForm'])->name('administrator.edit_course');
-        Route::put('admin/update-course', [CourseController::class, 'updateCourse'])->name('administrator.update_course');
-        Route::delete('admin/courses/delete/{courseId}', [CourseController::class, 'deleteCourse'])->name('administrator.delete_course');
-    
-    // Rutas para la gestión de asignaturas
-        Route::get('admin/subjects', [SubjectController::class, 'showSubjects'])->name('administrator.show_subjects');
-        Route::get('admin/subjects/add', [SubjectController::class, 'addSubjectForm'])->name('administrator.add_subject_form');
-        Route::post('admin/subjects/add', [SubjectController::class, 'addSubject'])->name('administrator.add_subject');
-        Route::get('admin/subjects/edit/{subjectId}', [SubjectController::class, 'showEditSubjectForm'])->name('administrator.edit_subject');
-        Route::put('admin/update-subjects', [SubjectController::class, 'updateSubject'])->name('administrator.update_subject');
-        Route::delete('admin/subjects/delete/{subjectId}', [SubjectController::class, 'deleteSubject'])->name('administrator.delete_subject');
-
+        // Rutas para la gestión de asignaturas
+        Route::prefix('admin/subjects')->group(function () {
+            Route::get('/', [SubjectController::class, 'showSubjects'])->name('administrator.show_subjects');
+            Route::get('add', [SubjectController::class, 'addSubjectForm'])->name('administrator.add_subject_form');
+            Route::post('add', [SubjectController::class, 'addSubject'])->name('administrator.add_subject');
+            Route::get('edit/{subjectId}', [SubjectController::class, 'showEditSubjectForm'])->name('administrator.edit_subject');
+            Route::put('update', [SubjectController::class, 'updateSubject'])->name('administrator.update_subject');
+            Route::delete('delete/{subjectId}', [SubjectController::class, 'deleteSubject'])->name('administrator.delete_subject');
+        });
     });
 
-
-    // TEACHER
+    // Rutas para el profesor
     Route::middleware('can:teacherAccess')->group(function () {
-    // Rutas para la gestión de cursos y asignaturas del profesor
-        Route::get('teacher/courses', [CourseController::class, 'showCoursesByTeacher'])->name('teacher.show_courses_by_teacher');
-        Route::get('teacher/courses/{courseId}/subjects', [CourseController::class, 'showSubjectsInCourse'])->name('teacher.show_subjects_in_course');
-        Route::get('teacher/users-in-subject/{subjectId}', [SubjectController::class, 'showUsersInSubject'])->name('teacher.show_users_in_subject');
+        // Rutas para la gestión de cursos y asignaturas del profesor
+        Route::prefix('teacher')->group(function () {
+            Route::get('courses', [CourseController::class, 'showCoursesByTeacher'])->name('teacher.show_courses_by_teacher');
+            Route::get('courses/{courseId}/subjects', [CourseController::class, 'showSubjectsInCourse'])->name('teacher.show_subjects_in_course');
+            Route::get('users-in-subject/{subjectId}', [SubjectController::class, 'showUsersInSubject'])->name('teacher.show_users_in_subject');
 
-    // Rutas para la gestión de notas
-        Route::get('teacher/add-notes/{subjectId}', [NoteController::class, 'showAddNotesForm'])->name('teacher.add_notes');
-        Route::post('teacher/save-notes', [NoteController::class, 'saveNotes'])->name('teacher.save_notes');
-        Route::get('teacher/edit-notes/{userId}/{subjectId}', [NoteController::class, 'showEditNotesForm'])->name('teacher.edit_notes');
-        Route::post('teacher/update-notes', [NoteController::class, 'updateNotes'])->name('teacher.update_notes');
-        Route::delete('teacher/delete-note', [NoteController::class, 'deleteNote'])->name('teacher.delete_note');
-    
-    // Rutas para la gestión de exámenes
-        Route::get('teacher/exams', [ExamController::class, 'showExams'])->name('teacher.show_exams');
-        Route::get('teacher/exams/create', [ExamController::class, 'createExam'])->name('teacher.create_exam');
-        Route::post('teacher/exams', [ExamController::class, 'storeExam'])->name('teacher.store_exam');
-        Route::get('teacher/exams/edit/{idExam}', [ExamController::class, 'editExam'])->name('teacher.edit_exam');
-        Route::put('teacher/exams/{exam}', [ExamController::class, 'updateExam'])->name('teacher.update_exam');
-        Route::delete('teacher/exams/{exam}', [ExamController::class, 'deleteExam'])->name('teacher.delete_exam');
-        Route::post('teacher/exams', [ExamController::class, 'saveExam'])->name('teacher.save_exam');
+            // Rutas para la gestión de notas
+            Route::prefix('notes')->group(function () {
+                Route::get('add/{subjectId}', [NoteController::class, 'showAddNotesForm'])->name('teacher.add_notes');
+                Route::post('save', [NoteController::class, 'saveNotes'])->name('teacher.save_notes');
+                Route::get('edit/{userId}/{subjectId}', [NoteController::class, 'showEditNotesForm'])->name('teacher.edit_notes');
+                Route::post('update', [NoteController::class, 'updateNotes'])->name('teacher.update_notes');
+                Route::delete('delete', [NoteController::class, 'deleteNote'])->name('teacher.delete_note');
+            });
 
+            // Rutas para la gestión de exámenes
+            Route::prefix('exams')->group(function () {
+                Route::get('/', [ExamController::class, 'showExams'])->name('teacher.show_exams');
+                Route::get('create', [ExamController::class, 'createExam'])->name('teacher.create_exam');
+                Route::post('store', [ExamController::class, 'storeExam'])->name('teacher.store_exam');
+                Route::get('edit/{idExam}', [ExamController::class, 'editExam'])->name('teacher.edit_exam');
+                Route::put('update/{exam}', [ExamController::class, 'updateExam'])->name('teacher.update_exam');
+                Route::delete('delete/{exam}', [ExamController::class, 'deleteExam'])->name('teacher.delete_exam');
+                Route::post('save', [ExamController::class, 'saveExam'])->name('teacher.save_exam');
+            });
 
-    // Recuperar asignaturas por curso
-        Route::get('teacher/courses/{courseId}/get-subjects-json', [CourseController::class, 'getSubjectJson']);
-
+            // Recuperar asignaturas por curso
+            Route::get('courses/{courseId}/get-subjects-json', [CourseController::class, 'getSubjectJson']);
+        });
     });
-    
 
-    // STUDENT
+    // Rutas para el estudiante
     Route::middleware('can:studentAccess')->group(function () {
-        // Asignaturas del Alumno
-        Route::get('student/subjects', [SubjectController::class, 'showSubjectsByStudent'])->name('student.show_subjects_by_student');
-
-        // Notas del Alumno para la asignatura
-        Route::get('student/notes/{subjectId}', [NoteController::class, 'showNotesBySubject'])->name('student.show_notes_by_subject');
+        Route::prefix('student')->group(function () {
+            // Asignaturas del Alumno
+            Route::get('subjects', [SubjectController::class, 'showSubjectsByStudent'])->name('student.show_subjects_by_student');
+            // Notas del Alumno para la asignatura
+            Route::get('notes/{subjectId}', [NoteController::class, 'showNotesBySubject'])->name('student.show_notes_by_subject');
+        });
     });
 });
